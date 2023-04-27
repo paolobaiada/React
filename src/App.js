f99f9b92f42ea30da342fc04bb57f1ee7e87e267
PaoloBaiada <paolosettemaggio@gmail.com>
2023-04-27 16:59:57 +0200

    extract message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Welcome from "./Components/Welcome";
+import Age from "./Components/Age";
 import React from "react";
 class App extends React.Component{
 
   render(){
-    return   <Welcome age = {22} name = {'Luca'} />
+    return  (
+<>
+<Welcome age = {22} name = {'Luca'} />
+<Age/>
+</>
+    )
+    
+    
        
      
     
---
+++ src/Components/Age.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Welcome from "./Welcome";
+class Age extends React.Component{
+render(){
+    return <p><Welcome age ={22}/></p>
+}
+}
+export default Age;
