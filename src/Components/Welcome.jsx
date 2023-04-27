ac7c8221bde70bc00e287ca59a70effb4b8c4a99
PaoloBaiada <paolosettemaggio@gmail.com>
2023-04-27 18:22:33 +0200

    props present

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -5,7 +5,7 @@ class Welcome extends React.Component{
     render(){
         
        
-        if (this.props.age > 18) {
+        if (this.props.age) {
             return <p>Welcome, {this.props.name}  your age is {this.props.age}</p>
         }
        
