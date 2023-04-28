43e5236ca10d66f9fc7b4aab3607e5fcb73ea61a
PaoloBaiada <paolosettemaggio@gmail.com>
2023-04-28 11:56:05 +0200

    prova commit

--- src/Components/Age.jsx
+++ src/Components/Age.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Welcome from "./Welcome";
+
 class Age extends React.Component{
 render(){
-    return <p><Welcome age={19}/></p>
+    return <p><Welcome age={20}/></p>
 }
 }
 export default Age;
--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 class Welcome extends React.Component{
     
     render(){
