c80b58e429f4b621a23bb22443c0b4c4796fea39
PaoloBaiada <paolosettemaggio@gmail.com>
2023-04-28 23:24:57 +0200

    jsx-expression

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 class App extends React.Component{
 
   render(){
-    return   <Welcome age = {22} name = {'Luca'} />
+    return   <Welcome age = {22} name = {<strong>Luca</strong>} />
        
      
     
