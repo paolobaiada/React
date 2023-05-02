bafc98e6bb296c735436320d08e2f9e35e8ced6c
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-02 15:11:33 +0200

    prova

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,21 +1,21 @@
 import {useState} from "react";
 
 function Counter(props){
-    let [count,setCount] = useState(0);
-  
-return (
-  <>
-  <h1>{props.count}</h1>
-  <button onClick={add}>Increment</button>
-  </>
-)
-function add(){
-  setInterval(() =>
-    setCount(count += props.incrementAmount),props.incrementIntervall
-   
-  )
-  return setCount;
-}
+    const [count,setCount] = useState(0);
 
-}
+    function add(){
+      setInterval(() =>
+        setCount(count + props.incrementAmount),props.incrementIntervall)
+    }
+
+    return (
+      <>
+      <h1>{count}</h1>
+      <button onClick={add}>Increment</button>
+      </>
+    )
+    }
+  
+
+
 export default Counter;
