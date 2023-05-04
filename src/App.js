180123d610d66dc3449e9b4037740fceb2f1f565
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-04 12:24:49 +0200

    prova

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import Welcome from "./Components/Welcome";
 import Age from "./Components/Age";
 import React from "react";
 import Counter from "./Components/Counter";
+import InteractiveWelcome from "./Components/InteractiveWelcome";
 class App extends React.Component{
 
   render(){
    
    return (
     <>
-     <Welcome age = {64} name = 'John'/> 
+      <Welcome age = {64} name = 'John'/>  
     <Age />
-    <Counter incrementAmount = {1} incrementIntervall = {1000}/>
+    {/* <Counter incrementAmount = {1} incrementIntervall = {1000}/> */}
+    
+    <InteractiveWelcome/>
     </>
    
    )
