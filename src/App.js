1687b509d8999357afa81205dd1a5da17e9b7b2f
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-10 15:10:36 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import ClickCounter from "./Components/ClickCounter";
   
   function App() {
     return (
-    <ClickCounter render={(onCounterChange) => {
-      function onCounterChange(count){
-        console.log("The Counter value is:", count)
-       }
+    <ClickCounter onCounterChange = {(count) => {
+      console.log("The Counter value is:", count);
     }}/>
-     )
+    )
     }
     
   
--- src/Components/ClickCounter.jsx
+++ src/Components/ClickCounter.jsx
@@ -1,23 +1,20 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
- const ClickCounter = (props) => {
-   const [count,setCount] = useState(0)
+const ClickCounter = (props) => {
+  const [count, setCount] = useState(0);
 
-   const handleCounter = () => {
-       setCount(count + 1)
-   }
-   function onCounterChange(count){
-    console.log("The Counter value is:", count)
-   }
-   useEffect(() => {
-  props.render(count,onCounterChange)
-    
-   },[count])
-return (
+  const handleCounter = () => {
+    setCount(count + 1);
+  };
+  
+  useEffect(() => {
+    props.onCounterChange(count);
+  });
+  return (
     <div>
-        <button onClick={handleCounter}>invio</button>
-       <h1>{count}</h1>
+      <button onClick={handleCounter}>invio</button>
+      <h1>{count}</h1>
     </div>
-)
-}
-export default ClickCounter
+  );
+};
+export default ClickCounter;
