aafa2f71a7d562eae8c40f34c91a9d051954d074
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-13 12:44:51 +0200

    usecallback

--- src/useCounter.jsx
+++ src/useCounter.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 
  const useCounter = () => {
     const [count,setCount] = useState(0)
 
-    const increment = () => {
+    const increment = useCallback(() => {
         setCount(count + 1)
-    }
+    },[count]) 
 
-    const decrement = () => {
+    const decrement = useCallback(() => {
         setCount(count - 1)
-    }
-    const reset = () => {
+    },[count]) 
+
+    const reset = useCallback(() => {
         setCount(0)
-    }
+    },[count]) 
     
     return{count,increment,decrement,reset}
     
