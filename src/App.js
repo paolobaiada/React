aeb20d6a22b0dd71338f7284b0bd7870e89e81ba
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-13 11:34:07 +0200

    carDetails

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CarDetails from "./Components/CarDetails";
   function App() {
     
     return (
-   <CarDetails brand = {"ferrari"} model={"458italia"} year={"2010"} color ={"red"}/>
+   <CarDetails brand = "ferrari" model="458italia" year= {2010} color="red"/>
     )
     }
     
--- src/Components/CarDetails.jsx
+++ src/Components/CarDetails.jsx
@@ -1,35 +1,64 @@
 import { useRef } from "react";
 import "../CarDetails.css";
+
 const CarDetails = (props) => {
-  const brand = useRef(props.brand);
+  const brand = useRef();
   const model = useRef();
   const year = useRef();
   const color = useRef();
+
+/* function handleChange(event){
+  if (event.target.name === "brand") {
+    brand.current.value = event.target.value
+const inputValue = brand.current.value
+console.log(inputValue)
+  }
+  else if (event.target.name === "model") {
+    model.current.value = event.target.value
+    const inputValue = model.current.value
+    console.log(inputValue)
+  }
+  else if (event.target.name === "year") {
+    year.current.value = event.target.value
+    const inputValue = year.current.value
+    console.log(inputValue)
+  }
+  else 
+    color.current.value = event.target.value
+    const inputValue = color.current.value
+    console.log(inputValue)
+  
+
+} */
+  
   return (
-    <form>
+    <div>
+  <form>
       <p>
         <label htmlFor="brand">Brand:</label>
-        <input type="text" name="brand" value={props.brand} />
+        <input type="text" name="brand" ref={brand} defaultValue={props.brand} />
       </p>
       <p>
-        <label htmlFor="model" name="model">
-          Model:
-        </label>
-        <input type="text" />
+        <label htmlFor="model">Model:</label>
+        <input type="text" name="model" ref={model} defaultValue={props.model} />
       </p>
       <p>
-        <label htmlFor="year" name="year">
-          Year:
-        </label>
-        <input type="text" />
+        <label htmlFor="year">Year:</label>
+        <input type="text" name="year" ref={year} defaultValue={props.year} />
       </p>
       <p>
-        <label htmlFor="color" name="color">
-          Color:
-        </label>
-        <input type="text" />
+        <label htmlFor="color">Color:</label>
+        <input type="text" name="color" ref={color} defaultValue={props.color} />
       </p>
     </form>
+    </div>
+  
   );
+};
+CarDetails.defaultProps = {
+  brand: "lamborghini",
+  model: "aventador",
+  yaer: "2015",
+  color: "yellow"
 };
 export default CarDetails;
