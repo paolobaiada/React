3930afa5c3ee6f0b72274299395b48f7a7af1550
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-17 19:21:36 +0200

    swr2

--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -1,18 +1,23 @@
 
-import useSWR from 'swr'
+import useSWR from 'swr';
 const useGithubUser = ({ username }) => {
+  
   const fetchData = async (url) => {
     try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data;
+      
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+      
+      
+     
     } catch (error) {
       console.log('Error:', error);
       throw error;
     }
   };
-
-  const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetchData);
+  
+  const { data, error } = useSWR(username ? `https://api.github.com/users/${username}` : null, fetchData);
 
   return {
     data,
