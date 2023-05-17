20d2f964b22aca5779e38fed2ab93ddcd84c6496
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-17 11:38:28 +0200

    prova

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Counter from "./Components/Counter";
+import ShowGithubUser from "./Components/ShowGithubUser";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Counter />} />
+      <Route path="/:username" Component={ShowGithubUser}/>
     </Routes>
   );
 }
---
+++ src/Components/ShowGithubUser.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router";
+import GithubUser from "./GithubUser";
+
+const ShowGithubUser = () => {
+    const {username = "paolo"} = useParams();
+    return (
+        <div>
+            <GithubUser username={username}/>
+        </div>
+    )
+}
+export default ShowGithubUser;
