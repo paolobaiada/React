531bfa7578c74f987d97e0e8e33d5d0886fcbdf0
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-17 14:26:50 +0200

    indice

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ShowGithubUser from "./Components/ShowGithubUser";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<h1>Aggiungi un utente e selezionalo</h1>}/>
       <Route path="/Counter" element={<Counter />} />
       <Route path="/ShowGithubUser" Component={<ShowGithubUser/>}/>
     </Routes>
