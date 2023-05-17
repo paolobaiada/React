5ec17e70394c693347c6d1988b5faae1ebd560c4
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-17 12:42:46 +0200

    prova

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { Route, Routes,Link, Router } from "react-router-dom";
+import { Route, Routes,Link } from "react-router-dom";
 import Counter from "./Components/Counter";
 import ShowGithubUser from "./Components/ShowGithubUser";
-import { link } from "fs";
-import { directive } from "@babel/types";
+
+
 
 function App() {
   return (
-<Router>
-  <link to="/">Counter</link>
- <link to= "/:username">ShowGithubUser</link>
+<div>
+  <Link to="/">Counter</Link>
+ <Link to= "/ShowGithubUser">ShowGithubUser</Link>
 <Routes>
       <Route path="/" element={<Counter />} />
-      <Route path="/:username" Component={<ShowGithubUser/>}/>
+      <Route path="/.ShowGithubUser" Component={<ShowGithubUser/>}/>
     </Routes>
-</Router>
+</div>
 
    
   );
