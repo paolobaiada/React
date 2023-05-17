a72e4ada80842c69ec7e188474be1e3ebca7adbb
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-17 12:45:56 +0200

    collegamenti

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import { Route, Routes,Link } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Counter from "./Components/Counter";
 import ShowGithubUser from "./Components/ShowGithubUser";
 
-
-
 function App() {
   return (
-<div>
-  <Link to="/">Counter</Link>
- <Link to= "/ShowGithubUser">ShowGithubUser</Link>
-<Routes>
-      <Route path="/" element={<Counter />} />
-      <Route path="/.ShowGithubUser" Component={<ShowGithubUser/>}/>
-    </Routes>
-</div>
-
-   
+    <div>
+      <Link to="/">Counter</Link>
+      <Link to="/ShowGithubUser">ShowGithubUser</Link>
+      <Routes>
+        <Route path="/" element={<Counter />} />
+        <Route path="/ShowGithubUser" element={<ShowGithubUser />} />
+      </Routes>
+    </div>
   );
 }
 
