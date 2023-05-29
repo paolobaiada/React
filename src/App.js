11f38e4bcfa9b754443c2f4668f3e25f1a267abd
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-29 10:43:16 +0200

    prova

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Route path="/" element={<h1>Aggiungi un utente e selezionalo</h1>}/>
       <Route path="/Counter" element={<Counter />} />
       <Route path="/ShowGithubUser" Component={<ShowGithubUser/>}/>
+      <Route index element={<h1>Aggiungi un utente e selezionalo</h1>}/>
     </Routes>
   );
 }
